97d417db7fc4fd00e5361fc818895f6e2193d201
mennaahmed00 <mennaahmad355@gmail.com>
2026-09-22 11:47:18 +0300

    Stage 1: signup and login routes working

--- Assignment_4/main.py
+++ Assignment_4/main.py
@@ -4,7 +4,12 @@ import os
 from contextlib import asynccontextmanager
 from dotenv import load_dotenv
 from supabase import create_client, Client
-from fastapi import FastAPI
+from fastapi import FastAPI,HTTPException, status
+from pydantic import BaseModel
+
+
+
+
 
 load_dotenv()
 
@@ -23,3 +28,43 @@ app= FastAPI(lifespan=lifespan)
 @app.get("/")
 def read_root():
     return {"status": "API is online"}
+
+
+class UserCredentials(BaseModel):
+    email: str
+    password: str
+
+@app.post("/auth/signup", status_code = status.HTTP_201_CREATED)
+
+def signup(credentials: UserCredentials):
+    try:
+        response = supabase.auth.sign_up({
+            "email": credentials.email,
+            "password": credentials.password
+        })
+        return response
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=str(e)
+        )
+@app.post("/auth/login")
+def login(credentials: UserCredentials):
+        try:
+            response = supabase.auth.sign_in_with_password({
+                "email": credentials.email,
+                "password": credentials.password
+            })
+
+            return{
+                "access_token": response.session.access_token,
+                "refresh_token": response.session.refresh_token
+            }
+        except Exception as e:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Invalid login credentials"
+            )
+        
+
+
